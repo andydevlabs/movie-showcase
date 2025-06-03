c262468c8cce01c3348cfcc1276c3c55f231f6aa
andydevlabs <avotiana@mail.hei.school>
2025-06-03 15:17:00 +0300

    feat: implement getTrendingMovies function

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import { useDebounce } from "react-use";
+import { getTrendingMovies, updateSearchCount } from "../lib/appwrite";
 
 const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_KEY;
@@ -18,37 +19,57 @@ const Section = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [movieList, setMovieList] = useState([]);
     const [debounceSearch, setDebounceSearch] = useState("");
+    const [trendingMovies, setTrendingMovies] = useState([]);
 
-    useDebounce(() => setDebounceSearch(searchInput), 500, [searchInput])
+    useDebounce(() => setDebounceSearch(searchInput), 700, [searchInput]);
+
+    const getMovies = async (query) => {
+        const endpoint = query
+            ? `${TMDB_API_BASE_URL}/search/movie?query=${encodeURIComponent(
+                  query
+              )}`
+            : `${TMDB_API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+        try {
+            setIsLoading(true);
+            const response = await fetch(endpoint, API_OPTIONS);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+            setMovieList(data.results);
+
+            if (query && data.results.length > 0) {
+                await updateSearchCount(query, data.results[0]);
+            }
+        } catch (error) {
+            console.error(error.message);
+            setErrorMessage("Error getting movies, please try again");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const loadTrendingMovies = async () => {
+        try {
+            const movies = await getTrendingMovies();
+            setTrendingMovies(movies);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     useEffect(() => {
-        const getMovies = async (query) => {
-            const endpoint = query
-                ? `${TMDB_API_BASE_URL}/search/movie?query=${encodeURIComponent(
-                      query
-                  )}`
-                : `${TMDB_API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
-
-            try {
-                setIsLoading(true);
-                const response = await fetch(endpoint, API_OPTIONS);
-                if (!response.ok) {
-                    throw new Error(`Response status: ${response.status}`);
-                }
-
-                const data = await response.json();
-                console.log(data);
-
-                setMovieList(data.results);
-            } catch (error) {
-                console.error(error.message);
-                setErrorMessage("Error getting movies, please try again");
-            } finally {
-                setIsLoading(false);
-            }
-        };
         getMovies(debounceSearch);
+        
     }, [debounceSearch]);
+
+    useEffect(() => {
+        loadTrendingMovies();
+    }, [])
 
     return (
         <>
@@ -65,7 +86,24 @@ const Section = () => {
                     />
                 </div>
             </div>
-            <div className="all-movies">
+
+            {trendingMovies.length > 0 && (
+                <section className="trending">
+                    <h2 className="px-10 text-center lg:text-start lg:text-5xl">
+                        Trending Movies
+                    </h2>
+                    <ul className="flex justify-center items-center">
+                        {trendingMovies.map((movie, index) => (
+                            <li key={movie.$id}>
+                                <p className="text-transparent">{index + 1}</p>
+                                <img src={movie.poster_url} alt={movie.title} className="scale-120"/>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            )}
+
+            <section className="all-movies">
                 <h2 className="px-10 text-center lg:text-start lg:text-5xl">
                     All Movies
                 </h2>
@@ -82,7 +120,7 @@ const Section = () => {
                         ))}
                     </ul>
                 )}
-            </div>
+            </section>
         </>
     );
 };
